053f41c4e0774472a0f6bee1c92e3afefdbef727
FrmofD <jh825327123@163.com>
2026-08-20 11:04:17 +0800

    shou

--- server3.js
+++ server3.js
@@ -1,20 +1,24 @@
 const http = require('http');
+let likes = 0;
 const server = http.createServer((request, response) => {
     let statusCode = 200;
     let contentType = 'text/html; charset=utf-8';
     let content = '';
+    if (request.method === 'GET') {
+        likes++;
+        console.log(`likes: ${likes}`);
+    }
     if (request.url === '/pikachu') {
         content = '<h1>pika pika</h1><br><img src="https://i.imgur.com/k5cfniM.png" alt="Pikachu">';
     }else if (request.url === '/sylveon') {
         content = '<h1>sylv sylv</h1><br><img src=""https://i.imgur.com/rKGgVEm.png" alt="Sylveon">';
     }else {
-        status.Code = 404;
         content = 'this pokemon is currently resting!'
     }
     response.writeHead(statusCode, {'content-Type': contentType});
     response.end(content);
 
 })
-server.listen(2000, () => {
-    console.log('successful load, http://localhost:2000');
+server.listen(1000, () => {
+    console.log('successful load, http://localhost:1000');
 })
